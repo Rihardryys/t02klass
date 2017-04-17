7f6795e63ef83df2a4221a9fce3e2edf6c2b5203
Rihardryys <Rihardryys@users.noreply.github.com>
2017-04-17 16:21:13 +0300

    Update Proov1.java

--- Proov1.java
+++ Proov1.java
@@ -1,12 +1,12 @@
 public class Proov1{
 	public static void main(String[] args){
-		Ristkylik laud1=new Ristkylik(2, 1);
-		Ristkylik laud2=new Ristkylik(1.5, 1);
-		Ristkylik laud3=new Ristkylik(2, 1.5);
-		System.out.println(laud1.pindala()+" "+laud1.ymberm66t());
-		System.out.println(laud2.pindala());
-		System.out.println(laud3.pindala());
-		System.out.println(laud1);
-		System.out.println(laud2.toString());
+		Kuup kuubik1=new Kuup(2, 2);
+		Kuup kuubik2=new Kuup(1.5, 1);
+		Kuup kuubik3=new Kuup(2, 1.5);
+
+		System.out.println(kuubik1.toString());
+		System.out.println(kuubik2.toString());
+		System.out.println(kuubik3.toString());
+		
 	}
 }
